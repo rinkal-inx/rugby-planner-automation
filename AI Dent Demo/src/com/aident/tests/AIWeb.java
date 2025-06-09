2fe25c581ca32ded29a99c08d814e6c8ca22ca78
rinkal-inx <rinkal.p@inheritx.com>
2025-06-09 18:52:12 +0530

    Scenario 6,7,8

--- AI Dent Demo/src/com/aident/tests/AIWeb.java
+++ AI Dent Demo/src/com/aident/tests/AIWeb.java
@@ -85,9 +85,9 @@ public class AIWeb {
             wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text(),'Profile Completion')]")));
 
             if (driver.getPageSource().contains("Profile Completion")) {
-                System.out.println("✅ Sign Up Test Passed: Successfully created account.");
+                System.out.println("BUG#10_✅ Sign Up Test Passed: Successfully created account.");
             } else {
-                System.out.println("❌ Sign Up Test Failed: Sign Up was not successful.");
+                System.out.println("BUG#10_❌ Sign Up Test Failed: Sign Up was not successful.");
             }*/
             
 // Scenario 2 -- Sign In with dentist added by Admin
@@ -371,7 +371,7 @@ public class AIWeb {
             
 //Scenario 5 - Reset Password  (BUG 12)
             
-            WebElement forgetPassword = driver.findElement(By.xpath("//button[normalize-space()='Forgot your password?']"));
+           /* WebElement forgetPassword = driver.findElement(By.xpath("//button[normalize-space()='Forgot your password?']"));
             forgetPassword.click();
             
             WebElement emailField = driver.findElement(By.xpath("//input[@id='amplify-id-:r8:']"));
@@ -427,10 +427,75 @@ public class AIWeb {
                 String toastMessage = toast.getText().toLowerCase().trim();
 
                 if (toastMessage.contains("password has been reset successfully.")) {
-                    System.out.println("✅ Sign Up Test Passed: Toast detected with text = " + toastMessage);
+                    System.out.println("BUG#12_✅ Sign Up Test Passed: Toast detected with text = " + toastMessage);
                 } else {
-                    System.out.println("❌ Sign Up Test Failed: Toast appeared, but text did not match expected. Actual = " + toastMessage);
-                }
+                    System.out.println("BUG#12_❌ Sign Up Test Failed: Toast appeared, but text did not match expected. Actual = " + toastMessage);
+                }*/
+                
+//Scenario 6 - 'Update profile' button  (BUG 33)
+                
+            /*WebElement email3 = driver.findElement(By.xpath("//input[@id='amplify-id-:r2:']"));
+            email3.sendKeys("mobdoctor@yopmail.com");
+            
+            WebElement password3 = driver.findElement(By.xpath("//input[@id='amplify-id-:r5:']"));
+            password3.sendKeys("Test@123");
+            
+            WebElement signIn3 = driver.findElement(By.xpath("//button[normalize-space()='Sign in']"));
+            signIn3.click();
+            
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1[normalize-space()='Landing Page']")));
+            
+            WebElement xrayPanel3 = driver.findElement(By.xpath("//button[normalize-space()='X-Ray']"));
+            xrayPanel3.click();
+            
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[contains(text(),'Select an X-ray image and submit to view and analy')]")));
+            
+            WebElement profileIcon3 = driver.findElement(By.xpath("//button[@aria-label='My account']"));
+            profileIcon3.click();
+            
+            WebElement myAcc3= driver.findElement(By.xpath("//li[1]"));
+            myAcc3.click();
+                
+            if (driver.getPageSource().contains("Update Profile")) {
+                System.out.println("BUG#33_✅ Update Profile button is visible.");
+            } else {
+                System.out.println("BUG#33_❌ Update profile button is not visible.");
+            }*/
+            
+//Scenario 7 - Non Registered User  (BUG 39)
+            
+            /*WebElement email4 = driver.findElement(By.xpath("//input[@id='amplify-id-:r2:']"));
+            email4.sendKeys("abcde@yopmail.com");
+            
+            WebElement password4 = driver.findElement(By.xpath("//input[@id='amplify-id-:r5:']"));
+            password4.sendKeys("Test@123");
+            
+            WebElement signIn4 = driver.findElement(By.xpath("//button[normalize-space()='Sign in']"));
+            signIn4.click(); 
+            
+            WebElement alert = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                    By.xpath("//div[@class='amplify-alert__body']")
+                ));
+
+                if (alert.getText().contains("500")) {
+                    System.out.println("BUG#39_✅ Error Message Detected: " + alert.getText());
+                } else {
+                    System.out.println("BUG#39_⚠️ Alert found, but message does not match.");
+                }*/
+            
+//Scenario 8 - WEB PAGE TITLE  (BUG 38)            
+            
+         /*   String actualTitle = driver.getTitle();
+            System.out.println("Page Title:" + actualTitle);
+
+            String expectedTitle = "Toothfit-Admin"; 
+            if (actualTitle.equals(expectedTitle)) {
+                System.out.println("BUG#38_✅ Page title is correct.");
+            } else {
+                System.out.println("BUG#38_⚠️ Page title mismatch. Found: " + actualTitle);
+            }*/
+            
+            
             
         } catch (Exception e) {
             System.out.println("❌ Test Failed: " + e.getMessage());
