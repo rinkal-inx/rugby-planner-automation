b1e0afa21e14daa56443e1d63d19eea0a3c2e58d
rinkal-inx <rinkal.p@inheritx.com>
2025-11-12 18:19:11 +0530

    Test Case 6 commit

--- rugby-planner-automation/src/test/java/com/rugby/planner/tests/LogoutTest.java
+++ rugby-planner-automation/src/test/java/com/rugby/planner/tests/LogoutTest.java
@@ -1,6 +1,5 @@
 package com.rugby.planner.tests;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import com.rugby.planner.base.BaseTest;
 import com.rugby.planner.config.ConfigReader;
@@ -15,25 +14,23 @@ public class LogoutTest extends BaseTest {
 	    LoginPage loginPage = new LoginPage(driver);
 	    loginPage.login(ConfigReader.getProperty("username"), ConfigReader.getProperty("password"));
 	    Thread.sleep(3000);
-
-    
-        LogoutPage logoutPage = new LogoutPage(driver);
-        logoutPage.clickLogoutLink();
-        Assert.assertTrue(logoutPage.isConfirmationModalDisplayed(),
-                "❌ Logout confirmation modal should be visible.");
-        
-        LogoutPage logoutPage2 = new LogoutPage(driver);
-        logoutPage2.clickLogoutButton();
-        Assert.assertTrue(logoutPage2.isConfirmationModalDisplayed(),
-        		"❌ Logout button modal should be visible.");
-   
-        LogoutPage logoutPage1 = new LogoutPage(driver);
-        logoutPage1.clickLogoutLink();
-        Assert.assertTrue(logoutPage1.isConfirmationModalDisplayed(),
-                "❌ Logout confirmation modal not displayed before confirming logout.");
-
-        logoutPage1.confirmLogout();
-        Assert.assertTrue(logoutPage1.isOnLoginPage(),
-                "❌ User not redirected to login page after logout.");
+	    
+	    LogoutPage logoutpage = new LogoutPage(driver);
+	    
+	    logoutpage.navigateToLogoutlink();
+	    System.out.println("Navigated to Logout dropdown");
+	    
+	    logoutpage.clickLogoutButton();
+	    System.out.println("Logout button clicked");
+	    
+	    logoutpage.displayconfirmationModal();
+	    System.out.println("Logout pop up appears");
+	    
+	    logoutpage.clickConfirmbutton();
+	    System.out.println("Logout confirmed");
+	    
+	    logoutpage.displaySigninPage();
+	    System.out.println("Signin page appeared");
+	    
 	 }
-    }
+}
